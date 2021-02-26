a0778b7bb45006f0eeee124cb8e69baf72626434
Ronaldo Lelis <delelis.locutor@gmail.com>
2021-02-25 21:11:10 -0300

    subindo o projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,48 @@
+
+// ---> ESSE CÓDIGO ORIGIANL NÃO ESTÁ FUNCIONANDO <---
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+// Demo's JS here
+window.addEventListener("load", function () {
+    var owlOptions = {
+      loop: false,
+      margin: 10,
+      nav: true,
+      navText: ['', ''],
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    };
+  
+    $('.owl-carousel').owlCarousel(owlOptions);
+  
+    // Bind click: use owlOnClick to ignore drag's click trigger    
+    owlOnClick($('.owl-carousel'), '.item', function (e) {
+      var $currentItem = $(e.target).closest('.item');
+      alert($currentItem[0].classList);
+    });
+  }, false);
